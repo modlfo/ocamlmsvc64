c1e8311e105db8d24311037b5b43e977685a297b
Leonardo Laguna Ruiz <modlfo@gmail.com>
2019-05-29 11:08:38 +0300

    Adds --no-setup to skip user input

--- appveyor-opam.sh
+++ appveyor-opam.sh
@@ -6,7 +6,7 @@ wget https://github.com/fdopen/opam-repository-mingw/releases/download/0.0.0.2/o
 tar -xf 'opam64.tar.xz'
 bash opam64/install.sh
 
-opam init default "https://github.com/fdopen/opam-repository-mingw.git#opam2" -c "ocaml-variants.4.07.1+msvc64" --disable-sandboxing --root "C:\\ocaml"
+opam init default "https://github.com/fdopen/opam-repository-mingw.git#opam2" -c "ocaml-variants.4.07.1+msvc64" --disable-sandboxing --no-setup --root "C:\\ocaml"
 eval $(opam config env --root "C:\\ocaml")
 
 opam install pla containers ppx_deriving ounit menhir yojson merlin qtest jbuilder ocaml-migrate-parsetree --root "C:\\ocaml"
