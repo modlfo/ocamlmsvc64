cb2bfb5ac763d2b21a7e9496bcc9155bcb7a53fe
Leonardo Laguna Ruiz <modlfo@gmail.com>
2019-05-29 11:30:16 +0300

    Removes more user input

--- appveyor-opam.sh
+++ appveyor-opam.sh
@@ -9,4 +9,4 @@ bash opam64/install.sh
 opam init default "https://github.com/fdopen/opam-repository-mingw.git#opam2" -c "ocaml-variants.4.07.1+msvc64" --disable-sandboxing --no-setup --root "C:\\ocaml"
 eval $(opam config env --root "C:\\ocaml")
 
-opam install pla containers ppx_deriving ounit menhir yojson merlin qtest jbuilder ocaml-migrate-parsetree --root "C:\\ocaml"
+opam install pla containers ppx_deriving ounit menhir yojson merlin qtest jbuilder ocaml-migrate-parsetree --root "C:\\ocaml" -y
