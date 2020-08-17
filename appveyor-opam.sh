b34629599700c32eee2b0b2c6b1fda8ab5ccd846
Leonardo Laguna Ruiz <modlfo@gmail.com>
2020-08-17 09:45:26 +0300

    Tries to build ocaml 4.09.1

--- appveyor-opam.sh
+++ appveyor-opam.sh
@@ -6,7 +6,8 @@ wget https://github.com/fdopen/opam-repository-mingw/releases/download/0.0.0.2/o
 tar -xf 'opam64.tar.xz'
 bash opam64/install.sh
 
-opam init default "https://github.com/fdopen/opam-repository-mingw.git#opam2" -c "ocaml-variants.4.07.1+msvc64" --disable-sandboxing --no-setup --root "C:\\ocaml"
+opam init default "https://github.com/fdopen/opam-repository-mingw.git#opam2" -c "ocaml-variants.4.09.1+msvc64" --disable-sandboxing --no-setup --root "C:\\ocaml"
 eval $(opam config env --root "C:\\ocaml")
 
-opam install pla containers ppx_deriving ounit menhir yojson merlin qtest jbuilder ocaml-migrate-parsetree --root "C:\\ocaml" -y
+opam install pla containers.2.8.1 ppx_deriving ounit menhir yojson merlin qtest jbuilder ocaml-migrate-parsetree ocamlformat.0.15.0 --root "C:\\ocaml" -y
+opam pin add ocaml-lsp-server https://github.com/ocaml/ocaml-lsp.git --root "C:\\ocaml" -y
