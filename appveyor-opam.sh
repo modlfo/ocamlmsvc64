f0432ef725f3f46187734828d863ed73d9ae89bd
Leonardo Laguna Ruiz <modlfo@gmail.com>
2020-08-17 09:50:04 +0300

    Update appveyor-opam.sh

--- appveyor-opam.sh
+++ appveyor-opam.sh
@@ -6,7 +6,7 @@ wget https://github.com/fdopen/opam-repository-mingw/releases/download/0.0.0.2/o
 tar -xf 'opam64.tar.xz'
 bash opam64/install.sh
 
-opam init default "https://github.com/fdopen/opam-repository-mingw.git#opam2" -c "ocaml-variants.4.09.1+msvc64" --disable-sandboxing --no-setup --root "C:\\ocaml"
+opam init default "git://github.com/fdopen/opam-repository-mingw.git#opam2" -c "ocaml-variants.4.09.1+msvc64" --disable-sandboxing --no-setup --root "C:\\ocaml"
 eval $(opam config env --root "C:\\ocaml")
 
 opam install pla containers.2.8.1 ppx_deriving ounit menhir yojson merlin qtest jbuilder ocaml-migrate-parsetree ocamlformat.0.15.0 --root "C:\\ocaml" -y
